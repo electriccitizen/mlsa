318976213621532a3c6e6f0a62c97bd6cd6bbcbe
broeker <tim@electriccitizen.com>
2019-06-26 20:11:03 -0500

    MT-00: module updates.

--- src/pages/redirect.js
+++ src/pages/redirect.js
@@ -3,8 +3,11 @@ let stateObj = {
       foo: "bar",
     };
 function RedirectPage() {
-  window.history.pushState(stateObj, "Google.com", "redirect")
-  window.location.href='http://www.google.com'
+  if (typeof window !== `undefined`) {
+    window.history.pushState(stateObj, "Google.com", "redirect")
+    window.location.href='http://www.google.com'
+  }
+
   return (
     <div>Error</div>
   );
