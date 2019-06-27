e186b87a7f8ca38b6b3fb98c2aad6343c63d4c0f
broeker <tim@electriccitizen.com>
2019-06-27 15:38:22 -0500

    MT-31: fix synax for enableBackground in svg button.

--- src/components/Escape/escape.js
+++ src/components/Escape/escape.js
@@ -70,7 +70,8 @@ class Escape extends React.Component {
          onClick={this.handleClick}
          className = "mr-16 bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 inline-flex items-center" >
           Escape
-          <svg className="fill-current text-white w-5 h-5 mr-2 ml-2"  xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 0 20" enableBackground="new 0 0 20 20" >
+
+          <svg className="fill-current text-white w-5 h-5 mr-2 ml-2"  xmlns="http://www.w3.org/2000/svg"  viewBox="0 0  20" enableBackground="new 0 0 20 20" >
             <path d="M14,10L8,5v3H1v4h7v3L14,10z M17,17H9v2h8c1.1,0,2-0.9,2-2V3c0-1.1-0.9-2-2-2H9v2h8V17z"/>
           </svg>
         </button>
