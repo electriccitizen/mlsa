2fa4f4306bc7d4fbe8f643321bfcf1d216889885
broeker <tim@electriccitizen.com>
2019-07-11 15:01:23 -0500

    MT-00: refactor after updates to page content type.

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,87 +1,25 @@
 import React from 'react';
-//import ParagraphText from '../ParagraphText/ParagraphText';
-//import ParagraphImage from '../ParagraphImage/ParagraphImage';
-//import ParagraphSection from '../ParagraphSection/ParagraphSection';
-//import Hero from '../Hero/Hero';
-
 class Page extends React.Component {
 
-  // renderElement(){
-  //   var media;
-  //   if (this.props.content) {
-  //   return (
-  //     <div>
-  //       {
-  //         this.props.content.map((item, key) => {
-  //
-  //           // if even, render grey background
-  //
-  //             if (item.__typename === 'paragraph__text') {
-  //               return (
-  //                 <ParagraphText
-  //                   title={item.__typename}
-  //                   text={item.field_text.processed}
-  //                   header={item.field_header}
-  //                   fx={item.field_special_fx}
-  //                 />
-  //               );
-  //             } else if (item.__typename === 'paragraph__image') {
-  //               if (item.relationships.field_single_image) {
-  //                 media = item.relationships.field_single_image.relationships.field_media_image
-  //               } else {
-  //                 media = ''
-  //               }
-  //               return (
-  //                 <ParagraphImage
-  //                   title={item.__typename}
-  //                   media={media}
-  //                   caption={item.field_caption.processed}
-  //                   fx={item.field_special_fx}
-  //                 />
-  //               );
-  //             } else if (item.__typename === 'paragraph__section') {
-  //               return (
-  //                 <ParagraphSection
-  //                   title="my section"
-  //                   content={item.relationships.field_content}
-  //                 />
-  //
-  //               );
-  //             } else {
-  //               return null
-  //             }
-  //
-  //         })
-  //       }
-  //     </div>
-  //   );
-  //   } else {
-  //     return null
-  //   }
-  //
-  //
-  // }
-
+  renderElement(){
+    if (this.props.content) {
+      return (
+        <div>
+         save this render element for future paragraphs logic
+        </div>
+      );
+    } else {
+      return null
+    }
+  }
 
   render() {
     return (
       <>
-        <h1>{this.props.hero.field_title}</h1>
-      {/*  <Hero*/}
-      {/*    title={this.props.hero.field_title}*/}
-      {/*    subtitle={this.props.hero.field_subheader}*/}
-      {/*    color={this.props.hero.field_hero_bg.color}*/}
-      {/*    opacity={this.props.hero.field_hero_bg.opacity}*/}
-      {/*    heroimage={this.props.hero.relationships.field_hero_image.relationships.field_media_image}*/}
-      {/*    hideheadline={this.props.hero.field_hide_headline}*/}
-      {/*    buttons={this.props.hero.relationships.field_buttons}*/}
-      {/*  />*/}
-      {/*{ this.renderElement() }*/}
+        <h1>{this.props.header.field_title}</h1>
       </>
     )
   }
 }
 
-
-
-export default withStyles(styles)(Page);
+export default Page;
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Layout from "../components/Layout/layout";
-import SEO from "../components/Layout/seo";
-
+import moment from 'moment'
+import Layout from '../components/Layout/layout';
 import Page from '../components/Page/Page';
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-});
 
 const pageTemplate = (props) => {
-  const { classes } = props;
   const { nodePage: page } = props.data;
 
   // Check for a valid media object and set to empty
@@ -25,18 +18,13 @@ const pageTemplate = (props) => {
   //}
 
   return (
-
-<Layout>
-      <SEO
-        title="Home"
-        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-      />
-      <Page
-          changed={'foo date'}
-          content={page.relationships.field_section}
-          hero={page.relationships.field_header}
+    <Layout>
+      <div>
+        <Page
+          title={'foo'}
+          header={page.relationships.field_header}
         />
-
+      </div>
     </Layout>
   )
 };
@@ -46,181 +34,48 @@ export default pageTemplate;
 export const query = graphql `
   query pageTemplate($drupal_id: String!) {
     nodePage(drupal_id: {eq: $drupal_id}) {
-    drupal_id
-    title
-    path {
-      alias
-    }
-    created
-    changed
-    relationships {
-      field_header {
-        field_title
-        field_color_mode
-        field_hide_headline
-        field_subheader
-        field_hero_bg {
-          color
-          opacity
-        }
-        relationships {
-          field_hero_image {
+      drupal_id
+      title
+      path {
+        alias
+      }
+      created
+      changed
+      relationships {
+        field_prefooter_image {
+          id
+         }
+         field_header {
             drupal_id
-            relationships {
-              field_media_image {
-                localFile {
-                  publicURL
-                  childImageSharp {
-                    fluid(
-                    maxWidth: 200, 
-                    duotone: {
-                    highlight: "#f00e2e",
-                    shadow: "#192550"
-                    }
-                    ) 
-                    {
-                      ...GatsbyImageSharpFluid
-                      
-                    }
-                  }
-                }
-              }
-            }
+            field_title
+            field_subheader
           }
-          field_buttons {
-            drupal_id 
-            field_button_text
-            field_icon
-            field_button_link {
-              uri
-              title
+          field_content {
+            __typename
+            ... on paragraph__accordion_group {
+              drupal_id
             }
-            field_button_size
-            field_button_style
-            field_button_color
-          }
-        }
-      }
-      field_section {
-        field_background_color {
-          color
-          opacity
-        }
-        __typename
-        ... on paragraph__section {
-          drupal_id
-          relationships {
-            field_content {
-              __typename
-              ... on paragraph__text {
-                drupal_id
-                field_special_fx
-                field_header
-                field_text {
-                  value
-                }
-              }
-              ... on paragraph__image {
-                drupal_id
-                field_special_fx
-                field_caption {
-                  value
-                }
-                relationships {
-                  field_single_image {
-                    drupal_id
-                    relationships {
-                      field_media_image {
-                        drupal_id
-                        filename
-                        uri {
-                          value
-                          url
-                        }
-                        localFile {
-                          publicURL 
-                          childImageSharp {
-                            fixed {
-                              base64
-                              tracedSVG
-                              aspectRatio
-                              width
-                              height
-                              src
-                              srcSet
-                              srcWebp
-                              srcSetWebp
-                              originalName
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-              ... on paragraph__grid {
-                drupal_id
-                field_columns
-                relationships {
-                  field_grid_content {
-                    __typename
-                    ... on paragraph__text {
-                      drupal_id
-                      field_special_fx
-                      field_header
-                      field_text {
-                        value
-                        processed
-                      }
-                    }
-                    ... on paragraph__image {
-                      drupal_id
-                      field_special_fx
-                      field_caption {
-                        value
-                      }
-                      relationships {
-                        field_single_image {
-                          drupal_id
-                          relationships {
-                            field_media_image {
-                              drupal_id
-                              filename
-                              uri {
-                                value
-                                url
-                              }
-                              localFile {
-                                publicURL 
-                                childImageSharp {
-                                  fixed {
-                                    base64
-                                    tracedSVG
-                                    aspectRatio
-                                    width
-                                    height
-                                    src
-                                    srcSet
-                                    srcWebp
-                                    srcSetWebp
-                                    originalName
-                                  }
-                                }
-                              }
-                            }
-                          }
-                        }
-                      }
-                    } 
-                  }
-                }
-              }
+            ... on paragraph__button {
+              drupal_id
             }
+            ... on paragraph__horizontal_rule {
+              drupal_id
+            }
+            ... on paragraph__image {
+              drupal_id
+            }
+            ...on paragraph__text {
+              drupal_id
+            }
+            ... on paragraph__text_with_image {
+              drupal_id
+            }
+            ... on paragraph__video {
+              drupal_id
+            }
+            
           }
         }
       }
     }
-  }
-}
 `;
