a60d2c7e8b7fc06b8f5649932e09d5500b064b3b
Adam Fuchs <adam@electriccitizen.com>
2019-07-12 14:20:17 -0500

    Type test

--- src/pages/type-test.js
+++ src/pages/type-test.js
@@ -10,7 +10,7 @@ function typeTest() {
         <h2>This is the H2 header.</h2>
         <h3>This is the H3 header.</h3>
         <h4>This is the H4 header.</h4>
-        <p className="text-13">This is some text with some <strong>bold type</strong> and some <em>italic type</em> and a <Link to="/">link in</Link> the middle.</p>
+        <p>This is some text with some <strong>bold type</strong> and some <em>italic type</em> and a <Link to="/">link in</Link> the middle.</p>
         <p className="text-15">This is some text with some <strong>bold type</strong> and some <em>italic type</em> and a <Link to="/">link in</Link> the middle.</p>
         <h4><Link>Morbi lobortis, libero a rhoncus eleifend, arcu ipsum fringilla ligula?</Link></h4>
         <p>Donec condimentum, sapien vitae bibendum volutpat, enim mi ullamcorper risus, molestie elementum orci massa feugiat diam. Mauris vitae neque. Praesent at odio at diam lobortis rutrum. Quisque eget turpis. Aenean purus dui, cursus vitae, porta id, pretium a, turpis.</p>
