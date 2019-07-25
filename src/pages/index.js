54916afc72fbbca1b4336b3194f7b6b51ba17d6a
Adam Fuchs <adam@electriccitizen.com>
2019-07-25 13:49:00 -0500

    Combine home page items

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout/layout";
 import SEO from "../components/Layout/seo";
+import HomeIntro from "../components/Block/home-intro";
 import HomeFeatured from '../components/Block/home-featured';
 
 function IndexPage() {
@@ -11,6 +12,7 @@ function IndexPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
       />
       <section className="text-center  md:justify-center">
+        <HomeIntro />
         <HomeFeatured />
       </section>
     </Layout>
