a8bb6dabe17eb0c4a9b935607958a0d05baa766d
broeker <tim@electriccitizen.com>
2019-07-25 12:21:03 -0500

    MT-60: random image test.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout/layout";
 import SEO from "../components/Layout/seo";
-import HomeFeatured from '../components/Block/home-featured';
-
+import CatAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+import HomeFeatured from '../components/Block/home-featured'
 function IndexPage() {
   return (
     <Layout>
@@ -12,6 +12,8 @@ function IndexPage() {
       />
       <section className="text-center  md:justify-center">
         <HomeFeatured />
+        <CatAndHumanIllustration className="block mx-auto w-1/2" title="Cat and human sitting on a couch"
+        />
       </section>
     </Layout>
   );
