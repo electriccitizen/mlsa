4aad8b0ba650fef55991c621c6218b4f7d492a01
Adam Fuchs <adam@electriccitizen.com>
2019-07-25 12:10:29 -0500

    Re-add home featued to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout/layout";
 import SEO from "../components/Layout/seo";
-import HomeIntro from '../components/Block/home-intro';
+import HomeFeatured from '../components/Block/home-featured';
 
 function IndexPage() {
   return (
@@ -10,9 +10,8 @@ function IndexPage() {
         title="Home"
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
       />
-      <section className="text-center md:justify-center">
-        <h1 className="visually-hidden">Find Help for Victims of Crime in Montana</h1>
-        <HomeIntro />
+      <section className="text-center  md:justify-center">
+        <HomeFeatured />
       </section>
     </Layout>
   );
