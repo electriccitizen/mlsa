151f2565d5060f5216e12c36b4e0bef03b46ab58
broeker <tim@electriccitizen.com>
2019-07-25 12:27:00 -0500

    MT-60: fix homepage merge.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Layout from "../components/Layout/layout";
 import SEO from "../components/Layout/seo";
 import HomeFeatured from '../components/Block/home-featured';
 
->>>>>>> 0bb1f46710bb142aa9cb99134e25ef0a019bd6b4
 function IndexPage() {
   return (
     <Layout>
