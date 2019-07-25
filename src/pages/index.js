7d7983dcbebe073ba6b005d1885afab15de53e47
broeker <tim@electriccitizen.com>
2019-07-25 12:27:00 -0500

    MT-60: fix homepage merge.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout/layout";
 import SEO from "../components/Layout/seo";
-import CatAndHumanIllustration from "../images/cat-and-human-illustration.svg";
-import HomeFeatured from '../components/Block/home-featured'
+import HomeFeatured from '../components/Block/home-featured';
+
 function IndexPage() {
   return (
     <Layout>
@@ -12,8 +12,6 @@ function IndexPage() {
       />
       <section className="text-center  md:justify-center">
         <HomeFeatured />
-        <CatAndHumanIllustration className="block mx-auto w-1/2" title="Cat and human sitting on a couch"
-        />
       </section>
     </Layout>
   );
