f04ff4ca167a26c50d9b4cb850f8dd83131a458f
Adam Fuchs <adam@electriccitizen.com>
2019-07-30 13:23:39 -0500

    Use correct iconObj

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -51,7 +51,7 @@ const flatten = arr =>
             category: relationships.category ? relationships.category.map(categoryObj => categoryObj.name) : null,
             county: relationships.county ? relationships.county.map(countyObj => countyObj.name) : null,
             type: relationships.type ? relationships.type.map(typeObj => (typeObj.name,typeObject.field_icon)) : null,
-            icon: relationships.type ? relationships.type.map(iconObj => (iconObject.field_icon)) : null,
+            icon: relationships.type ? relationships.type.map(iconObj => (iconObj.field_icon)) : null,
             ...field_description,
             ...field_url,
             ...relationships.type.field_icon,
