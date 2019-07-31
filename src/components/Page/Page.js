e52643496049e1ec50b63980ff3f244a60ed9219
Adam Fuchs <adam@electriccitizen.com>
2019-07-31 16:48:32 -0500

    Add basic page prefooter image

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,36 +1,44 @@
 import React from 'react';
 import Text from '../Paragraphs/text';
 import ReactComponent from '../Paragraphs/reactComponent';
+import Img from 'gatsby-image';
 
 class Page extends React.Component {
 
   render() {
     return (
       <>
-        <div class="text-center">
-          <h1>{this.props.header.field_title}</h1>
-          <h2 className="mx-auto max-w-3xl">{this.props.header.field_subheader}</h2>
-        </div>
-        <div>
-          {this.props.content.map((paragraphItem, index) => (
-            <div key={paragraphItem.drupal_id}>
-              {paragraphItem.__typename === 'paragraph__text' ? 
-                  <Text 
-                    header={paragraphItem.field_header}
-                    content={paragraphItem.field_text.processed} 
-                  />
-                : paragraphItem.__typename === 'paragraph__react_component' ?
-                  paragraphItem.relationships.field_components.map((reactItem, index) => (
-                    <ReactComponent 
-                      key={reactItem.id}
-                      content={reactItem.drupal_internal__tid}
+        <section className="max-w-1143 mx-auto px-4 py-8 md:px-7">
+          <div class="text-center">
+            <h1>{this.props.header.field_title}</h1>
+            <h2 className="mx-auto max-w-3xl">{this.props.header.field_subheader}</h2>
+          </div>
+          <div>
+            {this.props.content.map((paragraphItem, index) => (
+              <div key={paragraphItem.drupal_id}>
+                {paragraphItem.__typename === 'paragraph__text' ? 
+                    <Text 
+                      header={paragraphItem.field_header}
+                      content={paragraphItem.field_text.processed} 
                     />
-                  ))
-                : ''
-              }
-            </div>
-          ))}
-        </div>
+                  : paragraphItem.__typename === 'paragraph__react_component' ?
+                    paragraphItem.relationships.field_components.map((reactItem, index) => (
+                      <ReactComponent 
+                        key={reactItem.id}
+                        content={reactItem.drupal_internal__tid}
+                      />
+                    ))
+                  : ''
+                }
+              </div>
+            ))}
+          </div>
+        </section>
+        <section className="max-w-2280 mx-auto">
+          <div className="prefooter-wrapper">
+            <Img fluid={this.props.prefooter.relationships.field_single_image.relationships.field_media_image.localFile.childImageSharp.fluid} alt={this.props.prefooter.relationships.field_single_image.field_media_image.alt} />
+          </div>
+        </section>
       </>
     )
   }
--- src/templates/page.js
+++ src/templates/page.js
@@ -5,6 +5,7 @@ import Layout from '../components/Layout/layout';
 import Page from '../components/Page/Page';
 
 
+
 const pageTemplate = (props) => {
   const { nodePage: page } = props.data;
 
@@ -19,15 +20,11 @@ const pageTemplate = (props) => {
 
   return (
     <Layout>
-      <section className="max-w-1143 mx-auto px-4 py-8 md:px-7">
-        <Page
-          header={page.relationships.field_header}
-          content={page.relationships.field_content}
-        />
-      </section>
-      <section className="max-w-2280 mx-auto">
-        Place holder for prefooter image from page.js
-      </section>
+      <Page
+        header={page.relationships.field_header}
+        content={page.relationships.field_content}
+        prefooter={page.relationships.field_prefooter_image}
+      />
     </Layout>
   )
 };
@@ -47,52 +44,69 @@ export const query = graphql `
       relationships {
         field_prefooter_image {
           id
-         }
-         field_header {
-            drupal_id
-            field_title
-            field_subheader
-          }
-          field_content {
-            __typename
-            ... on paragraph__accordion_group {
-              drupal_id
-            }
-            ... on paragraph__button {
-              drupal_id
-            }
-            ... on paragraph__horizontal_rule {
-              drupal_id
-            }
-            ... on paragraph__image {
-              drupal_id
-            }
-            ... on paragraph__react_component {
-              drupal_id
-              drupal_internal__id
+          relationships {
+            field_single_image {
+              field_media_image {
+                alt
+              }
               relationships {
-                field_components {
-                  drupal_internal__tid
-                  id
+                field_media_image {
+                  localFile {
+                    childImageSharp {
+                      fluid(fit: COVER, maxWidth: 2280, jpegProgressive: true) {
+                        ...GatsbyImageSharpFluid
+                      }
+                    }
+                  }
                 }
               }
             }
-            ...on paragraph__text {
-              drupal_id
-              field_header
-              field_text {
-                processed
+          }
+        }
+        field_header {
+          drupal_id
+          field_title
+          field_subheader
+        }
+        field_content {
+          __typename
+          ... on paragraph__accordion_group {
+            drupal_id
+          }
+          ... on paragraph__button {
+            drupal_id
+          }
+          ... on paragraph__horizontal_rule {
+            drupal_id
+          }
+          ... on paragraph__image {
+            drupal_id
+          }
+          ... on paragraph__react_component {
+            drupal_id
+            drupal_internal__id
+            relationships {
+              field_components {
+                drupal_internal__tid
+                id
               }
             }
-            ... on paragraph__text_with_image {
-              drupal_id
-            }
-            ... on paragraph__video {
-              drupal_id
-            }
-            
           }
+          ...on paragraph__text {
+            drupal_id
+            field_header
+            field_text {
+              processed
+            }
+          }
+          ... on paragraph__text_with_image {
+            drupal_id
+          }
+          ... on paragraph__video {
+            drupal_id
+          }  
         }
       }
     }
+  }
 `;
