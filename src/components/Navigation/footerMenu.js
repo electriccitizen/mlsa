02aead963ff0447d241ec07a8afd83318dfd29a2
Adam Fuchs <adam@electriccitizen.com>
2019-08-01 15:59:29 -0500

    Set prefooter links to use alias field from taxonomy

--- src/components/Navigation/footerMenu.js
+++ src/components/Navigation/footerMenu.js
@@ -12,6 +12,7 @@ const FooterMenu = () => {
           node {
             id
             name
+            field_alias
             field_menu_link {
               uri
             }
@@ -25,8 +26,8 @@ const FooterMenu = () => {
     <nav className="text-center mb-4 md:text-left">
       {fMenu.allTaxonomyTermFooterMenu.edges.map(({ node }) => (
         <div key={node.id} className="footer-menu-item">
-          {String(node.field_menu_link.uri).includes('internal') ? <Link to="/">{node.name}</Link>
-            : String(node.field_menu_link.uri).includes('entity') ? <Link to="/">{node.name}</Link>
+          {String(node.field_menu_link.uri).includes('internal') ? <Link to={node.field_alias}>{node.name}</Link>
+            : String(node.field_menu_link.uri).includes('entity') ? <Link to={node.field_alias}>{node.name}</Link>
             : <a href={node.field_menu_link.uri} target="_blank" rel="noopener noreferrer">{node.name}</a>}
         </div>
       ))}
