1aae5e027757deed8337e5c0e832636ea9b3564a
Adam Fuchs <adam@electriccitizen.com>
2019-08-01 12:00:37 -0500

    Set smooth height transition on filter list

--- src/components/ResourceLibrary/filterToggle.js
+++ src/components/ResourceLibrary/filterToggle.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import FilterIcon from '../../images/filter.svg';
 import FilterGroup from './filterGroup';
+import AnimateHeight from 'react-animate-height';
 
 class FilterToggle extends React.Component {
   constructor() {
     super();
     this.state = {
-      shown: false,
+      height: 0,
     };
   } 
   
-  toggle() {
+  toggle = () => {
+    const { height } = this.state;
+ 
     this.setState({
-      shown: !this.state.shown
+      height: height === 0 ? 'auto' : 0,
     });
-  }
+  };
     
   render() {
-    var shown = this.state.shown ? "display-filters" : "";
+    const { height } = this.state;
+    let shown = this.state.height === 'auto' ? "true" : "false";
     
     return (
       <div className="lg:w-1/4 mb-10">
         <div className="lg:hidden m-auto max-w-xs relative">
-          <button className="bg-green-mid hover:bg-grey-dark focus:bg-grey-dark text-white text-20 text-left font-header font-extrabold px-4 py-3half w-full" onClick={this.toggle.bind(this)} aria-controls="resource-filter-list" aria-expanded={String(shown ? 'true' : 'false')}>
-            {shown ? 'Close Filters:' : 'Filters Results:'}
+          <button className="bg-green-mid hover:bg-grey-dark focus:bg-grey-dark text-white text-20 text-left font-header font-extrabold px-4 py-3half w-full" onClick={this.toggle.bind(this)} aria-controls="resource-filter-list" aria-expanded={String(shown)}>
+            {String(shown) === 'true' ? 'Close Filters:' : 'Filters Results:'}
             <FilterIcon className="filter-icon" />
           </button>
         </div>
@@ -31,13 +35,15 @@ class FilterToggle extends React.Component {
           Filter Results:
           <FilterIcon className="filter-icon" />
         </div>
-        <div id="resource-filter-list" className={`filter-list bg-white-dark pt-8 pb-6 px-3 ${shown}`} aria-hidden={String(shown ? 'false' : 'true')}>
-          <FilterGroup name="Crimes" attribute="crime" limit={20} />
-          <FilterGroup name="Related Issue" attribute="issue" limit={20} />
-          <FilterGroup name="Categories" attribute="category" limit={20} />
-          <FilterGroup name="Areas served" attribute="county" limit={60} />
-          <FilterGroup name="Resource Type" attribute="type" noscroll />
-        </div>
+        <AnimateHeight duration={500} height={height} id="resource-filter-list" aria-hidden={String(shown)}>
+          <div className={`filter-list bg-white-dark pt-8 pb-6 px-3 ${String(shown) === 'true' ? 'display-filters' : ''}`}>
+            <FilterGroup name="Crimes" attribute="crime" limit={20} />
+            <FilterGroup name="Related Issue" attribute="issue" limit={20} />
+            <FilterGroup name="Categories" attribute="category" limit={20} />
+            <FilterGroup name="Areas served" attribute="county" limit={60} />
+            <FilterGroup name="Resource Type" attribute="type" noscroll />
+          </div>
+        </AnimateHeight>
       </div>
     )
   }
