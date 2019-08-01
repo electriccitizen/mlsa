49be132fd92273c316fd1c41b4804e31a4e61893
Adam Fuchs <adam@electriccitizen.com>
2019-08-01 13:00:17 -0500

    Add other react components to component paragraph

--- src/components/Paragraphs/reactComponent.js
+++ src/components/Paragraphs/reactComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ResourceLibrary from "../ResourceLibrary/ResourceLibrary";
 import HomeResources from "../Block/homeResources";
+import HomeFeatured from "../Block/homeFeatured";
+import HomeIntro from "../Block/homeIntro";
 
 const searchIndices = [
   { name: `Resources`, title: `Resources`, hitComp: `ResourceHit` },
@@ -12,6 +14,8 @@ const ReactComponent = (props) => {
       {String(props.content) === '284' ? <ResourceLibrary collapse indices={searchIndices} />
       : String(props.content) === '285' ? 'placeholder for help guide'
       : String(props.content) === '286' ? <HomeResources />
+      : String(props.content) === '287' ? <HomeFeatured />
+      : String(props.content) === '288' ? <HomeIntro />
       : 'Sorry, there is no valid component matching your selection.'
     	}
     </div>
