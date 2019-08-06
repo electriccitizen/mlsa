413d3b46161831ddcf8f247f748776279416822c
broeker <tim@electriccitizen.com>
2019-08-06 15:37:31 -0500

    MT-32: initial state and localStorage.

--- src/pages/foo.js
+++ src/pages/foo.js
@@ -1,21 +1,20 @@
 import React from "react";
 import Layout from "../components/Layout/layout";
 import SEO from "../components/Layout/seo";
-import { Triage } from "../components/Triage/Triage"
+import { StateFoo } from "../components/StateFoo/StateFoo"
 
-function FindHelpPage() {
+function FooPage() {
   return (
     <Layout>
       <SEO
         title="About"
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
       />
-
-       <section className="max-w-1143 mx-auto px-4 py-8 md:px-7">
-        <Triage />
+      <section className="max-w-1143 mx-auto px-4 py-8 md:px-7">
+        <StateFoo />
       </section>
     </Layout>
   );
 }
 
-export default FindHelpPage;
+export default FooPage;
