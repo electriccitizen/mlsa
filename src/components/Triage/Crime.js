ba7dadef11ad498df2266b67237b80c4fcc1b2b6
broeker <tim@electriccitizen.com>
2019-08-07 10:06:38 -0500

    MT-32: refactors.

--- src/components/Triage/Crime.js
+++ src/components/Triage/Crime.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { useStaticQuery, graphql } from "gatsby"
+import { createContainer } from "unstated-next"
 import useLocalStorage from '../../hooks/use-local-storage';
+
+const Checkbox = ({ type = "checkbox", name, checked = false, onChange }) => {
+  console.log("Checkbox: ", name, checked);
+  return (
+    <input type={type} name={name} checked={checked} onChange={onChange} />
+  );
+};
 
 export function Crime(props) {
   //const [crime, setCrime] = useState(false);
   const [crime, setCrime] = useLocalStorage('crime', 'murder');
-
-
-  // const handleInputChange = (event) => {
-  //   const target = event.target;
-  //   const value = target.type === 'checkbox' ? target.checked : target.value;
-  //   const name = target.name;
-
-
 
   const data = useStaticQuery(graphql`
     query CrimeQuery {
@@ -26,13 +26,40 @@ export function Crime(props) {
         }
     }
   `)
+
+  const [checkedItems, setCheckedItems] = useLocalStorage('crime', '');
+  const handleChange = event => {
+    setCheckedItems({
+      ...checkedItems,
+      [event.target.name]: event.target.checked
+    });
+    console.log("checkedItems: ", checkedItems);
+  };
   console.log(props)
 
+  console.log(localStorage.getItem('crime'))
   return (
     <>
-      What is your crime? <input type="text" onChange={e => setCrime(e.target.value)}  name="crime" id="crime" value={crime} />
+      <ul class="mb-8">
+      {
+        data.allTaxonomyTermCrime.edges.map(
+          (term, index) =>
+            (
+              <li key={index}>
+              {/*<input onChange={e => setIntro(e.target.value)} type="checkbox" name={term.node.name} value={intro} />*/}
 
-      <a onClick={e => setCrime('')}>Reset</a>
+                <label>
+                <Checkbox
+                  name={term.node.name}
+                  checked={checkedItems[term.node.name]}
+                  onChange={handleChange}
+                />
+                {term.node.name}
+                </label>
+              </li>
+            )
+        )}
+      </ul>
       </>
 
   );
