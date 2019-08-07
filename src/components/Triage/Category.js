e6aaa285084c09f597ef7b41fd19f2814fc3ced1
broeker <tim@electriccitizen.com>
2019-08-07 10:06:38 -0500

    MT-32: refactors.

--- src/components/Triage/Category.js
+++ src/components/Triage/Category.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
 import { useStaticQuery, graphql } from "gatsby"
+import { createContainer } from "unstated-next"
+import useLocalStorage from '../../hooks/use-local-storage';
 
-export function Category() {
- const data = useStaticQuery(graphql`
+const Checkbox = ({ type = "checkbox", name, checked = false, onChange }) => {
+  console.log("Checkbox: ", name, checked);
+  return (
+    <input type={type} name={name} checked={checked} onChange={onChange} />
+  );
+};
+
+export function Category(props) {
+  //const [crime, setCrime] = useState(false);
+  const [crime, setCrime] = useLocalStorage('crime', 'murder');
+
+  const data = useStaticQuery(graphql`
     query CategoryQuery {
         allTaxonomyTermResourceCategory {
           edges {
@@ -15,18 +27,41 @@ export function Category() {
     }
   `)
 
+
+  const [checkedItems, setCheckedItems] = useLocalStorage('crime', '');
+  const handleChange = event => {
+    setCheckedItems({
+      ...checkedItems,
+      [event.target.name]: event.target.checked
+    });
+    console.log("checkedItems: ", checkedItems);
+  };
+  console.log(props)
+
+  console.log(localStorage.getItem('crime'))
   return (
-    <ul>
-      {
-        data.allTaxonomyTermResourceCategory.edges.map(
-          (term, index) =>
-            (
-              <li>
-              <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter" />
-                <label htmlFor="subscribeNews">{term.node.name}</label>
-              </li>
-            )
-        )}
-    </ul>
+    <>
+      <ul class="mb-8">
+        {
+          data.allTaxonomyTermResourceCategory.edges.map(
+            (term, index) =>
+              (
+                <li key={index}>
+                  {/*<input onChange={e => setIntro(e.target.value)} type="checkbox" name={term.node.name} value={intro} />*/}
+
+                  <label>
+                    <Checkbox
+                      name={term.node.name}
+                      checked={checkedItems[term.node.name]}
+                      onChange={handleChange}
+                    />
+                    {term.node.name}
+                  </label>
+                </li>
+              )
+          )}
+      </ul>
+    </>
+
   );
 }
--- src/components/Triage/Intro.js
+++ src/components/Triage/Intro.js
@@ -3,16 +3,12 @@ import { useStaticQuery, graphql } from "gatsby"
 import { createContainer } from "unstated-next"
 import useLocalStorage from '../../hooks/use-local-storage';
 
-
 const Checkbox = ({ type = "checkbox", name, checked = false, onChange }) => {
   console.log("Checkbox: ", name, checked);
-
   return (
     <input type={type} name={name} checked={checked} onChange={onChange} />
   );
 };
-
-
 
 export function Intro() {
  const data = useStaticQuery(graphql`
@@ -28,10 +24,10 @@ export function Intro() {
         }
     }
   `)
-  //const [intro, setIntro] = useState("");
 
   const [checkedItems, setCheckedItems] = useLocalStorage('intro', '');
-  const handleChange = event => {
+
+ const handleChange = event => {
     setCheckedItems({
       ...checkedItems,
       [event.target.name]: event.target.checked
@@ -40,50 +36,34 @@ export function Intro() {
   };
 
 
-  const [intro, setIntro] = useLocalStorage('intro', 'joebody');
+const [intro, setIntro] = useLocalStorage('intro', 'joebody');
  const [goo, setGoo] = useLocalStorage('goo', 'yo ma');
-console.log(localStorage.getItem('intro'))
+ console.log(localStorage.getItem('intro'))
   return (
     <>
-    <ul>
+    <ul className="mb-8">
       {
         data.allTaxonomyTermAudienceOptions.edges.map(
           (term, index) =>
             (
               <li key={index}>
               {/*<input onChange={e => setIntro(e.target.value)} type="checkbox" name={term.node.name} value={intro} />*/}
+
+                <label>
                 <Checkbox
                   name={term.node.name}
                   checked={checkedItems[term.node.name]}
                   onChange={handleChange}
                 />
-
-                <label>
-                {term.node.name}</label>
+                {term.node.name}
+                </label>
               </li>
             )
         )}
+      </ul>
 
-
-</ul>
-
-      <a onClick={e => setCheckedItems('')}>Reset</a>
+      {/*<a onClick={e => setCheckedItems('')}>Reset</a>*/}
 
       </>
   );
 }
-
-
-      {/*<ul>*/}
-    {/*  {*/}
-    {/*    data.allTaxonomyTermAudienceOptions.edges.map(*/}
-    {/*      (term, index) =>*/}
-    {/*        (*/}
-    {/*          <li key={index}>*/}
-    {/*          <input  onChange={e => setIntro(e.target.value)} type="checkbox" name={term.node.name} value={intro} />*/}
-    {/*            <label htmlFor="intro">{term.node.name}</label>*/}
-    {/*           {index}*/}
-    {/*          </li>*/}
-    {/*        )*/}
-    {/*    )}*/}
-    {/*</ul>*/}
