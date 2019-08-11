23e13979b7349a756acbfe751a9e3abe2eeef432
broeker <tim@electriccitizen.com>
2019-08-11 13:54:28 -0500

    MT-00: icon fix.

--- src/components/ResourceLibrary/hitComps.js
+++ src/components/ResourceLibrary/hitComps.js
@@ -12,17 +12,17 @@ import Info from '../../images/info.svg';
 export const ResourceHit = clickHandler => ({ hit }) => (
   <div className="text-center mb-8 md:text-left">
     <a className="resource-link" href={hit.url} onClick={clickHandler}>
-    	<span>
-    		<span className="visually-hidden">{hit.icon[0]}</span>
-        {hit.icon[0] === 'advocate' ? <Advocate className="resource-icon" />
-        : hit.icon[0] === 'database' ? <Database className="resource-icon" />
-        : hit.icon[0] === 'learn' ? <Learn className="resource-icon" />
-        : hit.icon[0] === 'question' ? <Question className="resource-icon q-icon" />
-        : hit.icon[0] === 'search' ? <Form className="resource-icon" />
-        : hit.icon[0] === 'toolbox' ? <Toolbox className="resource-icon" />
-        : hit.icon[0] === 'info' ? <Info className="resource-icon" />
-        : '' }
-    	</span>
+    	{/*<span>*/}
+    	{/*	<span className="visually-hidden">{hit.icon[0]}</span>*/}
+      {/*  {hit.icon[0] === 'advocate' ? <Advocate className="resource-icon" />*/}
+      {/*  : hit.icon[0] === 'database' ? <Database className="resource-icon" />*/}
+      {/*  : hit.icon[0] === 'learn' ? <Learn className="resource-icon" />*/}
+      {/*  : hit.icon[0] === 'question' ? <Question className="resource-icon q-icon" />*/}
+      {/*  : hit.icon[0] === 'search' ? <Form className="resource-icon" />*/}
+      {/*  : hit.icon[0] === 'toolbox' ? <Toolbox className="resource-icon" />*/}
+      {/*  : hit.icon[0] === 'info' ? <Info className="resource-icon" />*/}
+      {/*  : '' }*/}
+    	{/*</span>*/}
     	<h2 className="h3 mb-4 text-blue underline">
   	    <CustomHighlight attribute="title" hit={hit} />
       </h2>
