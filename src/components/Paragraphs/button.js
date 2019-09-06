2f08a59ee8e5bf952dfb0601955bc0bd33504ac9
Adam Fuchs <adam@electriccitizen.com>
2019-09-06 13:24:32 -0500

    Replace link uri with alias

--- src/components/Paragraphs/button.js
+++ src/components/Paragraphs/button.js
@@ -8,8 +8,9 @@ const Button = (props) => {
       <div className="flex flex-row flex-wrap justify-center -mx-2 -mb-4">
 	      {props.content.map((buttonLink, index) => (
 	      	<div className="px-2 mb-4" key={index}>
-	      		{String(buttonLink.uri).includes('entity') ? <Link className="btn inline-block" to={buttonLink.uri}>{buttonLink.title}</Link>
-	            : <a className="btn inline-block" href={buttonLink.uri} target="_blank" rel="noopener noreferrer">{buttonLink.title}</a>}
+	      		{String(buttonLink.alias).includes('http') ? <a className="btn inline-block" href={buttonLink.uri} target="_blank" rel="noopener noreferrer">{buttonLink.title}</a>
+              : String(buttonLink.alias) === '/user' ? <Link className="btn inline-block" to="/">{buttonLink.title}</Link>
+	            : <Link className="btn inline-block" to={buttonLink.alias}>{buttonLink.title}</Link>}
 					</div>
 	      ))}
       </div>
