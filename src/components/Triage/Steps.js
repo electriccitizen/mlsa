243dc0fd19c98f22a4370d076879ebd53023e59f
broeker <tim@electriccitizen.com>
2019-09-12 12:49:48 -0500

    MT-106: small fixes.

--- src/components/Triage/Steps.js
+++ src/components/Triage/Steps.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Responses } from "./Responses"
 import { Buttons } from "./Buttons"
 import { StepperWrapper } from "./Stepper"
