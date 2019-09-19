b352fa344466e0171545d72d9a101996c994302d
Adam Fuchs <adam@electriccitizen.com>
2019-09-19 12:25:56 -0500

    Refactor counties select

--- src/components/Triage/Steps/County.js
+++ src/components/Triage/Steps/County.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useStaticQuery, graphql } from "gatsby"
 import useLocalStorage from '../../../hooks/use-local-storage';
-import Select from 'react-select'
+import SelectElement from '../../Forms/select';
 
 export function County() {
 
@@ -41,41 +41,8 @@ export function County() {
     });
   };
 
-  const customStyles = {
-    control: (styles, state) => ({ 
-      ...styles, 
-      borderRadius: 0,
-      borderColor: state.isFocused ? '#979797' : '#979797',
-      boxShadow: state.isFocused ? '0 0 2px #1155cc' : 0,
-      '&:hover': {
-        borderColor: state.isFocused ? '#979797' : '#979797',
-      }
-    }),
-    dropdownIndicator: (styles, state) => ({ 
-      ...styles, 
-      color: '#1f2225',
-    }),
-    option: (provided, state) => ({
-      ...provided,
-      color: state.isSelected ? '#1155cc' : '#1f2225',
-      backgroundColor: state.isSelected ? "#ffffff" : provided,
-      padding: '8px 20px',
-      ':hover':{
-        backgroundColor: '#eeeeee',
-      },
-      ':focus':{
-        backgroundColor: '#eeeeee',
-      }
-    }),
-    singleValue: (provided, state) => {
-      const opacity = state.isDisabled ? 0.5 : 1;
-      const transition = 'opacity 300ms';
-      return { ...provided, opacity, transition };
-    }
-  }
-
   const CountySearch = () => (
-    <Select className="max-w-xs mb-12 text-15" styles={customStyles} value={selected} onChange={handleChange} options={options} />
+    <SelectElement classes="max-w-xs mb-12" name="county" selected={selected} placeholder="Select County" onChange={handleChange} options={options} />
   )
 
   return (
