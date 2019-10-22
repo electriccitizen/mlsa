5b2db861732bc8ee5e491351496d96a3b6aeee66
Adam Fuchs <adam@electriccitizen.com>
2019-10-22 13:21:59 -0500

    Sort internal resources to be first in triage, refactor results item for resources and triage

---
+++ src/components/Triage/resultHitContent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Advocate from '../../images/advocate.svg';
+import Database from '../../images/database.svg';
+import Learn from '../../images/learn.svg';
+import Question from '../../images/question.svg';
+import Form from '../../images/form.svg';
+import Toolbox from '../../images/toolbox.svg';
+import Info from '../../images/info.svg';
+import CustomHighlight from '../ResourceLibrary/highlight';
+
+const ResultHitContent = (props) => {
+  return (
+    <>
+      <span>
+        <span className="visually-hidden">{props.icon}</span>
+        {props.icon === 'advocate' ? <Advocate className="resource-icon" />
+        : props.icon === 'database' ? <Database className="resource-icon" />
+        : props.icon === 'learn' ? <Learn className="resource-icon" />
+        : props.icon === 'question' ? <Question className="resource-icon q-icon" />
+        : props.icon === 'search' ? <Form className="resource-icon" />
+        : props.icon === 'toolbox' ? <Toolbox className="resource-icon" />
+        : props.icon === 'info' ? <Info className="resource-icon" />
+        : '' }
+      </span>
+      <h2 className="h3 mb-4 text-blue underline force-left">
+        {props.title}
+        {props.highlight && 
+          <CustomHighlight attribute="title" hit={props.hit} />
+        }
+      </h2>
+    </>
+  );
+}
+
+export default ResultHitContent;
