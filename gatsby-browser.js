44b89c418b70a890b6f2e70784665d550b93c215
broeker <tim@electriccitizen.com>
2019-10-30 15:18:07 -0500

    MT-105: scroll improvements for find-help.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -16,7 +16,12 @@ exports.shouldUpdateScroll = ({
                                 routerProps: { location },
                               }) => {
     if (location.pathname === '/resource-library' && location.search) {
-      var element = document.querySelector("#searchbox");
+      let element = document.querySelector("#searchbox");
+      element.scrollIntoView()
+      return false
+    }
+    if (location.pathname === '/find-help' && location.hash !== '#step-1') {
+      let element = document.querySelector("#stepper");
       element.scrollIntoView()
       return false
     }
