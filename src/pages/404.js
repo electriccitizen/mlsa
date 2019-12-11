f869df42f3761156d06587bf81d8ad054b3b46cd
Adam Fuchs <adam@electriccitizen.com>
2019-12-11 12:49:22 -0600

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,35 @@
 import React from "react";
 import Layout from "../components/Layout/layout";
 import SEO from "../components/Layout/seo";
-import AbductionIllustration from "../images/abduction-illustration.svg";
+import { useStaticQuery, graphql, Link } from "gatsby";
+import Img from 'gatsby-image';
 
 function NotFoundPage() {
+  const lostPage = useStaticQuery(graphql`
+    query lostPage {
+      imageOne: file(relativePath: { eq: "montana-road.jpg" }) {
+        childImageSharp {
+          fluid(maxHeight: 350, maxWidth: 1080, jpegProgressive: true, fit: COVER) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <div>
+      <div className="px-4 py-8 md:px-7">
         <h1>Page not found!</h1>
-        <AbductionIllustration className="block mx-auto w-1/2" />
-        <p className="text-center text-22">Oh no—You've taken somewhere bad.</p>
+        <h2 className="text-center">Looks like you've taken a wrong turn.</h2>
+        <div className="mx-auto mb-12 max-w-2xlHalf">
+          <Img className="mb-6" fluid={lostPage.imageOne.childImageSharp.fluid} alt="Road into Montana foothills"/>
+          <p>Please use the main navigation, search for a resource in the <Link to="/resource-library">resource library</Link>, or use our <Link to="/find-help#step-1">help tool</Link> to find the content you were looking for</p>
+        </div>
       </div>
     </Layout>
   );
 }
 
 export default NotFoundPage;
+
