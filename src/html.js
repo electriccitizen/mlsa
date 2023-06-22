5fd2173ca2220abe44f2105517d8b0c90e2e4460
david kirkwood <david@electriccitizen.com>
2023-06-22 10:56:06 -0500

    MT-197A: For testing, hardcoded google analytics plugin script to html.js--original branch not deploying properly.

--- src/html.js
+++ src/html.js
@@ -24,6 +24,18 @@ export default function HTML(props) {
           id="___gatsby"
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
+        <script dangerouslySetInnerHTML={{__html: `
+              if(true) {
+                  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+              }
+              if (typeof ga === "function") {
+                  ga('create', 'UA-151582273-1', 'auto', {});
+              }
+        `} }
+        />
         {props.postBodyComponents}
         <script
           dangerouslySetInnerHTML={{__html: `
