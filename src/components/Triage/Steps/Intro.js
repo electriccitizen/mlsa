9b5295fe13b308af84db498abea614ca0f6f96a1
broeker <tim@electriccitizen.com>
2023-06-27 13:01:31 -0500

    fix for localstorage

--- src/components/Triage/Steps/Intro.js
+++ src/components/Triage/Steps/Intro.js
@@ -1,52 +1,49 @@
 import React from 'react';
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from 'gatsby';
 import useLocalStorage from '../../../hooks/use-local-storage';
 import Radio from '../../Forms/radio';
 
-export function Intro() {
+const isBrowser = typeof window !== 'undefined';
 
+export function Intro() {
   const data = useStaticQuery(graphql`
     query IntroQuery {
-        allTaxonomyTermAudienceOptions {
-          edges {
-            node {
-              drupal_id
-              name
-              drupal_internal__tid
-            }
+      allTaxonomyTermAudienceOptions {
+        edges {
+          node {
+            drupal_id
+            name
+            drupal_internal__tid
           }
         }
+      }
     }
-  `)
+  `);
 
-  console.log(data)
+  console.log(data);
 
   const [checkedItems, setCheckedItems] = useLocalStorage('intro', '');
 
   const handleChange = event => {
     setCheckedItems({
-      [event.target.name]: event.target.checked
+      [event.target.name]: event.target.checked,
     });
   };
 
   return (
-    <ul className="mb-8">
-      {
-        data.allTaxonomyTermAudienceOptions.edges.map(
-          (term, index) =>
-            (
-              <li key={index}>
-                <label className="radio">
-                  <Radio
+      <ul className="mb-8">
+        {data.allTaxonomyTermAudienceOptions.edges.map((term, index) => (
+            <li key={index}>
+              <label className="radio">
+                <Radio
                     name={term.node.name}
-                    checked={checkedItems[term.node.name]}
+                    checked={isBrowser && checkedItems[term.node.name]}
                     onChange={handleChange}
-                  />
+                />
                 <span>{term.node.name}</span>
-                </label>
-              </li>
-            )
-        )}
+              </label>
+            </li>
+        ))}
       </ul>
   );
 }
