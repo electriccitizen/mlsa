69799a8c0b5fc3024b6f74221c303619809a495b
broeker <tim@electriccitizen.com>
2023-06-28 14:20:03 -0500

    Add GA4

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,5 +84,29 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-151582273-1", // Google Analytics / GA
+            "G-HP0BH69LJ3"
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
   ]
 };
